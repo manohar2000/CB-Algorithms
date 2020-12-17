9ad46d8b6a7a83c8abcc15773f7756be3a162af7
manohar <maohar502@gmail.com>
2020-12-17 16:15:16 +0530

    adding insertion sort

---
+++ Sorting and Searching/insertion_sort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+
+void insertionsort(int arr[], int n)
+{
+    int min = arr[0];
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[i])
+            {
+                swap(arr[i],arr[j]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cin.get();
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    insertionsort(arr,n);
+        for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+
+}
