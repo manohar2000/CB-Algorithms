3189959bd730332acbc46dc295fb22efe5d12663
manohar <maohar502@gmail.com>
2020-12-20 23:14:58 +0530

    partioning array around a pivot

---
+++ Sorting and Searching/partioning.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// Aim : to modify the array in-place(without making a new array) so that all the elements <= pivot are on the left
+// and all the elements >= pivot are in the right side
+
+
+void partition(int arr[],int n,int p) 
+{   
+    int i = 0;
+    int j = 0;
+
+    while(i<n)
+    {
+        if(arr[i]>p) i++;
+        else 
+        {
+            swap(arr[i],arr[j]);
+            i++;
+            j++;
+        }
+    }
+
+}
+
+
+int main()
+{
+    int n, pivot;
+    cin>>n>>pivot;
+    int arr[n];
+    
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    partition(arr,n,pivot);
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
