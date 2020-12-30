d99caa78393109ae88c22a065bfcf01d1e884bd2
manohar <maohar502@gmail.com>
2020-12-30 15:52:46 +0530

    balance parenthesis

---
+++ Stacks and Queue/balancedparenthesis.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isBalanced(string str)
+{
+    stack <char> s;
+
+    for(int i=0;i<str.size();i++)
+    {
+        if(str[i]=='(') s.push('(');
+
+        else if(str[i]==')')
+        {
+            if(s.empty()) return false;
+            else s.pop();
+        }
+    }
+
+    return s.empty();
+}
+
+
+int main()
+{
+    string s;
+    cin>>s;
+    cout<<isBalanced(s);  
+    return 0;
+}
